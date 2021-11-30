067c6664ca42301297973e4e7384ea5cebc2cac2
LenaRib <poddubnayaeo@gmail.com>
2021-11-30 21:37:42 +0100

    feat: css and adaptive sub button

--- src/js/script.js
+++ src/js/script.js
@@ -4,10 +4,8 @@
     $(".menu__body").toggleClass("active");
     $("body").toggleClass("lock");
   });
-
   function ibg() {
     $.each($(".ibg"), function (index, val) {
-
       if ($(this).find("img").length > 0) {
         $(this).css(
             'background-image',
